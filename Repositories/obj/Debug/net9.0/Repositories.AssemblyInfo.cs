36a0224a7b814c9b1c8485fbd6b9a26b9aa908c3
Zehra Nur Olcay <101907759+zehranurolcay@users.noreply.github.com>
2025-12-22 21:42:33 +0300

    product delete added

--- Repositories/obj/Debug/net9.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net9.0/Repositories.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9df67700b3f234c979218bc14257b9fcdbd5b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b28817ec626c32e146b9a76aeb3d24863f2448a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
