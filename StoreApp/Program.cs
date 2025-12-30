b3154649e495993ca70b55d5b80aa679e7a00203
Zehra Nur Olcay <101907759+zehranurolcay@users.noreply.github.com>
2025-12-30 19:42:42 +0300

    enable session support for user-based cart and demo pages

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -14,18 +14,24 @@ builder.Services.AddDbContext<RepositoryContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("sqlconnection"),
     b =>b.MigrationsAssembly("StoreApp"));
 });
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
 builder.Services.AddScoped<IRepositoryManager,RepositoryManager>();
 builder.Services.AddScoped<IProductRepository,ProductRepository>();
 builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
 builder.Services.AddScoped<IServiceManager,ServiceManager>();
 builder.Services.AddScoped<IProductService,ProductManager>();
 builder.Services.AddScoped<ICategoryService,CategoryManager>();
+
 builder.Services.AddSingleton<Cart>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 app.UseStaticFiles();
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseRouting();
 app.UseEndpoints(endpoints =>
