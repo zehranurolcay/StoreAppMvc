d0abec15443de954056225c1be7e9eeb49a3025a
Zehra Nur Olcay <101907759+zehranurolcay@users.noreply.github.com>
2026-02-10 23:35:56 +0300

    add user registration

--- Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net9.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9703b4b03d6ff61bf2526bb5c016c05fc9742b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b15fd4d26e8e05f106dd10122a4a9f1ece8136")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StoreApp/Controllers/AccountController.cs
+++ StoreApp/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using Entities.Dtos;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StoreApp.Models;
@@ -9,14 +10,14 @@ namespace StoreApp.Controllers
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
 
-        public AccountController(UserManager<IdentityUser> userManager, 
+        public AccountController(UserManager<IdentityUser> userManager,
         SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
         }
 
-        public IActionResult Login([FromQuery(Name ="ReturnUrl")] string ReturnUrl = "/")
+        public IActionResult Login([FromQuery(Name = "ReturnUrl")] string ReturnUrl = "/")
         {
             return View(new LoginModel()
             {
@@ -31,24 +32,58 @@ namespace StoreApp.Controllers
             if (ModelState.IsValid)
             {
                 IdentityUser user = await _userManager.FindByNameAsync(model.Name);
-                if(user is not null)
+                if (user is not null)
                 {
                     //oturum açma
                     await _signInManager.SignOutAsync();
-                    if((await _signInManager.PasswordSignInAsync(user, model.Password, false, false)).Succeeded)
+                    if ((await _signInManager.PasswordSignInAsync(user, model.Password, false, false)).Succeeded)
                     {
                         return Redirect(model?.ReturnUrl ?? "/");
                     }
                 }
-                ModelState.AddModelError("Error","Invalid username or password.");
+                ModelState.AddModelError("Error", "Invalid username or password.");
             }
             return View();
         }
 
-        public async Task<IActionResult> Logout([FromQuery(Name ="ReturnUrl")] string ReturnUrl = "/")
+        public async Task<IActionResult> Logout([FromQuery(Name = "ReturnUrl")] string ReturnUrl = "/")
         {
             await _signInManager.SignOutAsync();
             return Redirect(ReturnUrl);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register([FromForm] RegisterDto model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await _userManager
+            .CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                var roleResult = await _userManager
+                .AddToRoleAsync(user, "User");
+                if (roleResult.Succeeded)
+                {
+                    return RedirectToAction("Login", new {ReturnUrl ="/"});
+                }
+            }
+            else
+            {
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View();
         }
 
     }
