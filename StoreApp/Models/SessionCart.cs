a765ecee59e6e9361ac8288a80b89d1a32244592
Zehra Nur Olcay <101907759+zehranurolcay@users.noreply.github.com>
2026-02-13 19:13:25 +0300

    improve session cart implementation

--- StoreApp/Models/SessionCart.cs
+++ StoreApp/Models/SessionCart.cs
@@ -2,39 +2,53 @@ using System.Text.Json.Serialization;
 using Entities.Models;
 using StoreApp.Infrastructure.Extensios;
 
-namespace  StoreApp.Models
+namespace StoreApp.Models
 {
     public class SessionCart : Cart
     {
         [JsonIgnore]
         public ISession? Session { get; set; }
+        private string? _key;
+
+        public SessionCart() { }
+        public SessionCart(string? key)
+        {
+            _key = key;
+        }
 
         public static Cart GetCart(IServiceProvider services)
         {
-            ISession? session = services.GetRequiredService<IHttpContextAccessor>()
-            .HttpContext?.Session;
+            var httpContext = services
+        .GetRequiredService<IHttpContextAccessor>()
+        .HttpContext;
 
-            SessionCart cart = session?.GetJson<SessionCart>("cart") ?? new SessionCart();
+            ISession? session = httpContext?.Session;
+
+            var userName = httpContext?.User?.Identity?.Name ?? "guest";
+            var key = $"cart_{userName}";
+
+            SessionCart cart = session?.GetJson<SessionCart>(key) ?? new SessionCart(key);
             cart.Session = session;
+            cart._key = key;   // bunu birazdan ekleyeceğiz
             return cart;
         }
 
         public override void AddItem(Product product, int quantity)
         {
             base.AddItem(product, quantity);
-            Session?.SetJson<SessionCart>("cart",this);
+            Session?.SetJson(_key!, this);
         }
 
         public override void Clear()
         {
             base.Clear();
-            Session?.Remove("cart");
+            Session?.Remove(_key!);
         }
 
         public override void RemoveLine(Product product)
         {
             base.RemoveLine(product);
-            Session?.SetJson<SessionCart>("cart",this);
+            Session?.SetJson(_key!, this);
         }
     }
 }
